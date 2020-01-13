c410502ef37564e44b634dd66c23fc7481fcd9f8
karolfavero <karol.faverob@gmail.com>
2020-01-13 11:35:26 -0300

    atualizando

--- app/src/main/java/com/example/adiligencia/MainActivity.java
+++ app/src/main/java/com/example/adiligencia/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.adiligencia;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 2000);
+    }
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this, telaprincipal.class));
+        finish();
     }
 
     public void experimente(View view){
