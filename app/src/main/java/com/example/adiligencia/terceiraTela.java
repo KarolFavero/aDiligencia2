6204109fa7d0e7e66cd32caaa0133e8279eb6110
karolfavero <karol.faverob@gmail.com>
2020-01-13 09:09:33 -0300

    atualizando

--- app/src/main/java/com/example/adiligencia/terceiraTela.java
+++ app/src/main/java/com/example/adiligencia/terceiraTela.java
@@ -15,7 +15,6 @@ public class terceiraTela extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-        getSupportActionBar().setTitle("Unisinos");
     }
 
 }
