f2b877c828f6a220a25b9967689356f594c4320d
karolfavero <karol.faverob@gmail.com>
2020-01-14 10:07:47 -0300

    atualizado

--- app/src/main/java/com/example/adiligencia/MainActivity.java
+++ app/src/main/java/com/example/adiligencia/MainActivity.java
@@ -2,27 +2,20 @@ package com.example.adiligencia;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements Runnable {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Handler handler = new Handler();
-        handler.postDelayed(this, 2000);
+
     }
 
-    @Override
-    public void run() {
-        startActivity(new Intent(this, telaprincipal.class));
-        finish();
-    }
 
     public void experimente(View view){
         Intent intent = new Intent(this, segundaTela. class);
--- app/src/main/java/com/example/adiligencia/telaprincipal.java
+++ app/src/main/java/com/example/adiligencia/telaprincipal.java
@@ -1,16 +1,29 @@
 package com.example.adiligencia;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class telaprincipal extends AppCompatActivity {
+public class telaprincipal extends AppCompatActivity implements Runnable {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.telaprincipal);
+
+        Handler handler = new Handler();
+        handler.postDelayed(this, 2000);
+
+
+    }
+
+    @Override
+    public void run() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 
 }
