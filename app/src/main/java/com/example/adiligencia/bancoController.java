6a950e3f2b7ec0ad4d8d947879fae850a48be9a2
karolfavero <karol.faverob@gmail.com>
2020-01-21 08:42:46 -0300

    Add Cursor 21.01

--- app/src/main/java/com/example/adiligencia/bancoController.java
+++ app/src/main/java/com/example/adiligencia/bancoController.java
@@ -2,7 +2,12 @@ package com.example.adiligencia;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import static com.example.adiligencia.criaBanco.EMAIL;
+import static com.example.adiligencia.criaBanco.SENHA;
+import static com.example.adiligencia.criaBanco.TABELA;
 
 public class bancoController {
 
@@ -23,7 +28,7 @@ public class bancoController {
         valores.put(criaBanco.EMAIL, email);
         valores.put(criaBanco.SENHA, senha);
 
-        resultado = db.insert(criaBanco.TABELA, null, valores);
+        resultado = db.insert(TABELA, null, valores);
         db.close();
 
         if (resultado == -1)
@@ -31,5 +36,18 @@ public class bancoController {
         else
             return "Registro Inserido com sucesso";
 
+    }
+
+    public Cursor fazerLogin(String email, String senha) {
+        db = banco.getWritableDatabase();
+        String sql = " SELECT * FROM " + TABELA + " WHERE " + EMAIL + " = ? AND " + SENHA + " = ? ";
+        String[] selectionArgs = new String[]{email, senha};
+        Cursor cursor = db.rawQuery(sql, selectionArgs);
+        if (cursor != null) {
+            cursor.moveToFirst();
+            return cursor;
+        } else {
+            return null;
+        }
     }
 }
