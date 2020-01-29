e1dd5063eb519e103bb73af48721115f8b2c630f
karolfavero <karol.faverob@gmail.com>
2020-01-29 10:08:57 -0300

    WebService add 29.01

--- app/src/main/java/com/example/adiligencia/CEP.java
+++ app/src/main/java/com/example/adiligencia/CEP.java
@@ -1,18 +1,70 @@
 package com.example.adiligencia;
 
-class CEP {
-    private String codibge;
-    private String codestado;
+public class CEP {
+
+    private String cep;
     private String logradouro;
     private String complemento;
-    private String cep;
     private String bairro;
     private String cidade;
     private String estado;
 
-    public String getCodibge() {
-        return codibge;
+    public String getCep() {
+        return cep;
     }
 
-    public Void
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "CEP: " + getCep()
+                + "\nLogradouro: " + getLogradouro()
+                + "\nComplemento: " + getComplemento()
+                + "\nBairro: " + getBairro()
+                + "\nCidade:" + getCidade()
+                + "\nEstado: " + getEstado();
+    }
+
 }
--- app/src/main/java/com/example/adiligencia/WebService1.java
+++ app/src/main/java/com/example/adiligencia/WebService1.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.ExecutionException;
 
 public class WebService1 extends AppCompatActivity {
 
@@ -15,12 +18,20 @@ public class WebService1 extends AppCompatActivity {
         setContentView(R.layout.activity_web_service1);
 
         final EditText etCep = findViewById(R.id.cep);
+        final TextView tvResposta = findViewById(R.id.resposta);
 
-        Button botao = findViewById(R.id.buscarCep);
-        botao.setOnClickListener(new View.OnClickListener() {
+        Button buscarCep = findViewById(R.id.buscarCep);
+
+        buscarCep.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (etCep.getText().toString().length() > 0 && etCep.getText().toString().equals("") && etCep.getText().toString().length() == 8) {
+                try {
+                    CEP retorno = new HTTPService(etCep.getText().toString()).execute().get();
+                    tvResposta.setText(retorno.toString());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
 
                 }
             }
